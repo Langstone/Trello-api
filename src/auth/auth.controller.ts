842f80fb35ec2b4a3061874ed626be2af41aa989
Langstone <kalipsomart@gmail.com>
2024-01-31 06:30:38 +0600

    added description for registration and login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/users/dto/create.user.dto';
 import { AuthService } from './auth.service';
 
@@ -9,11 +9,13 @@ export class AuthController {
 
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({summary: 'Login user'})
   @Post('/login')
   login(@Body() userDto: CreateUserDto) {
     return this.authService.login(userDto);
   }
 
+  @ApiOperation({summary: 'Create user'})
   @Post()
   registration(@Body() userDto: CreateUserDto) {
     return this.authService.registration(userDto);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,13 +12,6 @@ import { Request } from 'express';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @ApiOperation({summary: 'Create user'})
-  @ApiResponse({status: 200, type: User})
-  // @Post()
-  // create(@Body() userDto: CreateUserDto) {
-  //   return this.usersService.createUser(userDto);
-  // }
-
   @ApiOperation({summary: 'Get all users'})
   @ApiResponse({status: 200, type: [User]})
   @UseGuards(JwtAuthGuard)
